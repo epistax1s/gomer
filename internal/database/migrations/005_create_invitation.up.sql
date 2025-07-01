c29085ba9a7f251ba2e46445d2a301d6dc098954
epistax1s <kir_9n@yahoo.com>
2025-07-01 22:44:53 +0700

    fix: invitation migration

--- internal/database/migrations/005_create_invitation.up.sql
+++ internal/database/migrations/005_create_invitation.up.sql
@@ -3,11 +3,11 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS invitation (
     id INTEGER NOT NULL,
     'code' TEXT NOT NULL UNIQUE,
-    'created_by_user_id' INTEGER NOT NULL,
     'created_at' TEXT NOT NULL,
-    'used_by_user_id' INTEGER,
+    'created_by_user_id' INTEGER NOT NULL,
+    'used' INTEGER NOT NULL,
     'used_at' TEXT,
-    'is_active' INTEGER NOT NULL DEFAULT 1,
+    'used_by_user_id' INTEGER,
     CONSTRAINT invitation_pk PRIMARY KEY (id),
     CONSTRAINT invitation_created_by_fk FOREIGN KEY ('created_by_user_id') REFERENCES tg_user(id),
     CONSTRAINT invitation_used_by_fk FOREIGN KEY ('used_by_user_id') REFERENCES tg_user(id)
